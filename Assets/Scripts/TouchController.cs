535028e447bc2ea5c66f366a40fcf558bff87039
Alex Chen <achen115@ucr.edu>
2021-12-06 01:31:09 -0800

    fix compile error

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -1,20 +1,14 @@
 ﻿using UnityEngine;
-
 public class TouchController : MonoBehaviour {
-
     public OVRInput.Controller controller;
     private GameObject player;
     private Vector3 playerPos;
-    private void Start()
-    {
+    private void Start() {
         player = GameObject.Find("OVRPlayerController");
         playerPos = player.transform.position;
     }
-
     void Update () {
-
         transform.localPosition = OVRInput.GetLocalControllerPosition(controller);
         transform.localRotation = OVRInput.GetLocalControllerRotation(controller);
-
     }
 }
